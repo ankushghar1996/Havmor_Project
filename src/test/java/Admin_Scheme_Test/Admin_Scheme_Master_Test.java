94ef2638c0c0608dac797850dc217c26e803ead6
AniKetHSPL <aniket.jadhav@heerasoftware.com>
2025-09-29 10:22:36 +0530

    Add new Scheme

--- src/test/java/Admin_Scheme_Test/Admin_Scheme_Master_Test.java
+++ src/test/java/Admin_Scheme_Test/Admin_Scheme_Master_Test.java
@@ -1,6 +1,12 @@
 package Admin_Scheme_Test;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import Admin_Scheme_Main.Admin_Scheme_Master_Main;
 import Admin_Target_Upload_Main.Program_Target_Upload_Main;
@@ -12,7 +18,7 @@ import Com_Utility_Havmor.ObjectRepo_Havmor;
 public class Admin_Scheme_Master_Test extends BaseClass{
 
 	@Test
-    public void Program_Target_Upload_Page() throws Exception {
+    public void Scheme_Creation() throws Exception {
 
 		Admin_Scheme_Master_Main ASMM = PageFactory.initElements(driver, Admin_Scheme_Master_Main.class);
         HomePage_Havmor home = PageFactory.initElements(driver , HomePage_Havmor.class);
@@ -52,11 +58,74 @@ public class Admin_Scheme_Master_Test extends BaseClass{
             Liabrary_Havmor.custom_Sendkeys(ASMM.getScheme_Short_Name_Textbox_EXCEL(), excel.getStringdata2("Scheme", 1, 1), "Scheme Short Name");
              });
             Thread.sleep(1000);
+	
+    
+         // From Date
+            selectDate(driver, 
+              By.id("ctl00_ParentMasterContentPlaceHolder1_dtpFromDate_dateInput"), 
+              "26", "September", "2025");
+
+            // To Date
+            selectDate(driver, 
+              By.id("ctl00_ParentMasterContentPlaceHolder1_dtpToDate_dateInput"), 
+              "28", "December", "2025");
+
             
+            
+            
+            // Example usage of date picker:
+        
+	}         
+        
+
+	public void selectDate(WebDriver driver, By calendarInput, String expDay, String expMonth, String expYear) throws Exception {
+	    // 1️⃣ Click the date input to open the calendar
+	    driver.findElement(calendarInput).click();
+	    Thread.sleep(500); // tiny wait
+
+	    // 2️⃣ Loop until correct month/year appears in header
+	    while (true) {
+	        String monthYear = driver.findElement(
+	                By.xpath("(//div[@id='ctl00_ParentMasterContentPlaceHolder1_dtpFromDate_calendar_wrapper']//table//*[contains(@class,'rcTitle')])[1]")).getText(); 
+	        // e.g. "September 2025"
+
+	        if (monthYear.contains(expMonth) && monthYear.contains(expYear)) {
+	            break; // we’re on the right month/year
+	        } else {
+	            // click next arrow
+	            driver.findElement(
+	                By.xpath("//a[contains(@class,'rcNext')]")).click();
+	            Thread.sleep(300);
+	        }
+	    }
+
+	    // 3️⃣ Now click the correct day
+	    List<WebElement> allDays = driver.findElements(
+	        By.xpath("//table[contains(@class,'rcMainTable')]//td[not(contains(@class,'rcOutOfRange'))]/a"));
+	    // this ignores disabled/out-of-range cells
+
+	    for (WebElement day : allDays) {
+	        if (day.getText().equals(expDay)) {
+	            day.click();
+	            break;
+	        }
+	    }
+	}
+
+            
+
+
+
+
+
+
+
+
+
             
             
                	
            	
 	
-	}
+	
 }
