44eb296ab102079134f1d25b54ac93a31bd00395
AniKetHSPL <aniket.jadhav@heerasoftware.com>
2025-10-29 09:49:48 +0530

    Add 29 oct

--- src/test/java/SE_Portal_Test/Deactivation_Approval_Test.java
+++ src/test/java/SE_Portal_Test/Deactivation_Approval_Test.java
@@ -10,47 +10,27 @@ import SE_Portal.Deactivation_Approval_Main;
 
 public class Deactivation_Approval_Test extends BaseClass_SE{
 
-	
-	
 	@Test
-	public void Deactivation_Approval() throws Exception {
-	
-	
+	public void Deactivation_Approval() throws Exception {	
 	Deactivation_Approval_Main app = PageFactory.initElements(driver , Deactivation_Approval_Main.class);
-	
 	
 	ObjectRepo_Havmor.startTestAndLog_1_SS("Deactivation_Approval_Test_01", "Verify that user Click On Master Menu", () ->{
 	Liabrary_Havmor.custom_click(app.getMaster_Menu(), "Click On Master Menu");      
 	});
 	Thread.sleep(1000);
-	
-	
+		
 	ObjectRepo_Havmor.startTestAndLog_1_SS("Deactivation_Approval_Test_02", "Verify that user Click On Retailer Master SubMenu", () ->{
 	Liabrary_Havmor.custom_click(app.getRetailer_Master_SubMenu(), "Click On Retailer Master SubMenu");      
 	});
 	Thread.sleep(1000);
-	
-	
+		
 	ObjectRepo_Havmor.startTestAndLog_1_SS("Deactivation_Approval_Test_03", "Verify that user Click On Retailer Approval", () ->{
 	Liabrary_Havmor.custom_click(app.getDeactivation_Approval(), "Click On Retailer Approval");      
 	});
 	Thread.sleep(1000);
 
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+ 
 	}
 	
 }
